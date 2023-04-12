a9622935e34c3b49eeb2a9ba051c84711fa6584c
wx69wx <94267729+wx69wx@users.noreply.github.com>
2023-04-12 11:43:31 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -32,7 +32,7 @@ module.exports = {
   googleFontSans: 'Inter',
   // googleFontMono - the monospace font used in onedrive-vercel-index.
   googleFontMono: 'Fira Mono',
-  // googleFontLinks -  an array of links for referencing the google font assets.
+  // googleFontLinks -  an array of links for referencing the google font assets..
   googleFontLinks: ['https://fonts.googleapis.com/css2?family=Fira+Mono&family=Inter:wght@400;500;700&display=swap'],
 
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
